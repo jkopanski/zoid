fb60f4ba9eb49098b7793a306f5462c7aceba4f0
Jakub Kopanski <J.Kopanski@imio.pw.edu.pl>
2016-07-14 17:30:05 +0200

    utility functions for instruction parameters extraction

--- src/Decode.hs
+++ src/Decode.hs
@@ -4,26 +4,109 @@ import CLaSH.Prelude hiding (Word)
 import Types
 import ISA
 
+-- instructionTypes = [ (Load,       rtype)
+--                    , (LoadFp,     rtype)
+--                    , (Custom0,    rtype)
+--                    , (MiscMem,    rtype)
+--                    , (OpImm,      rtype)
+--                    , (Auipc,      rtype)
+--                    , (OpImm32,    rtype)
+--                    , (VLIW48a,    rtype)
+--                    , (Store,      rtype)
+--                    , (StoreFp,    rtype)
+--                    , (Custom1,    rtype)
+--                    , (Amo,        rtype)
+--                    , (Op,         rtype)
+--                    , (Lui,        rtype)
+--                    , (Op32,       rtype)
+--                    , (VLIW64,     rtype)
+--                    , (MAdd,       rtype)
+--                    , (MSub,       rtype)
+--                    , (NMSub,      rtype)
+--                    , (NMAdd,      rtype)
+--                    , (OpFp,       rtype)
+--                    , (Rederved0,  rtype)
+--                    , (Custom2,    rtype)
+--                    , (VLIW48b,    rtype)
+--                    , (Branch,     rtype)
+--                    , (JalR,       rtype)
+--                    , (Reserved1,  rtype)
+--                    , (Jal,       ujType)
+--                    , (System,     rtype)
+--                    , (Reserved2,  rtype)
+--                    , (Custom3,    rtype)
+--                    , (VLIW80,     rtype)
+--                    ]
+
+fstSourceReg :: Word -> Register
+fstSourceReg (W w) = slice d19 d15 w
+
+sndSourceReg :: Word -> Register
+sndSourceReg (W w) = slice d24 d20 w
+
+destReg :: Word -> Register
+destReg (W w) = slice d11 d7 w
+
+getFunct3 :: Word -> Funct3
+getFunct3 (W w) = slice d14 d12 w
+
+getFunct7 :: Word -> Funct7
+getFunct7 (W w) = slice d31 d25 w
+
+getIimm :: Word -> Iimmediate
+getIimm (W w) = slice d31 d20 w
+
+getSimm :: Word -> Simmediate
+getSimm (W w) = a ++# b
+  where a = slice d31 d25 w
+        b = slice d11 d7 w
+
+getSBimm :: Word -> SBimmediate
+getSBimm (W w) = a ++# b ++# c ++# d
+  where a = msb w
+        b = w ! 7
+        c = slice d30 d25 w
+        d = slice d11 d8 w
+
+getUimm :: Word -> Uimmediate
+getUimm (W w) = slice d31 d12 w
+
+getUJimm :: Word -> UJimmediate
+getUJimm (W w) = a ++# b ++# c ++# d
+  where a = msb w
+        b = slice d19 d12 w
+        c = w ! 20
+        d = slice d30 d21 w
+
 decode :: Word -> Instruction
-decode (W w) =
-  let (rest, aa) = split w :: (BitVector 30, BitVector 2) in
+decode bits@(W w) =
   case aa of
-    0b11 -> decodeWord rest
+    0b11 -> decodeBase bits
     otherwise -> error "Other instruction encoding lenghts are currently unsupported"
+  where aa = slice d1 d0 w
 
-decodeWord :: BitVector 30 -> Instruction
-decodeWord w =
-  let (rest, opcode) = split w :: (BitVector 25, BitVector 5)
-      op = toEnum $ fromIntegral $ toInteger opcode
-   in arguments op rest
+decodeBase :: Word -> Instruction
+decodeBase = undefined
+-- decodeBase :: BitVector 30 -> Instruction
+-- decodeBase w =
+--   let (rest, opcode) = split w :: (BitVector 25, BitVector 5)
+--       op = toEnum $ fromIntegral $ toInteger opcode
+--    in arguments op rest
 
+-- let dest    = slice d4  d0
+--     funct3  = slice d7  d5
+--     source1 = slice d12 d8
+--     source2 = slice d17 d13
+--     funct7  = slice d18 d25 in
+-- decodeR :: BitVector 25 -> Instruction
+-- decodeR w = 
 
-arguments :: Operation -> BitVector 25 -> Instruction
-arguments Jal w = JAL (unpack dest) offset
-  where
-    (imm, dest) = split w :: (BitVector 20, BitVector 5)
-    imm3 = msb imm
-    imm2 = slice d7 d0 imm
-    imm1 = imm ! 8
-    imm0 = slice d18 d9 imm
-    offset = unpack $ imm3 ++# imm2 ++# imm1 ++# imm0
+-- arguments :: Operation -> BitVector 25 -> Instruction
+-- arguments Jal w = JAL (unpack dest) offset
+--   where
+--     (imm, dest) = split w :: (BitVector 20, BitVector 5)
+--     imm3 = msb imm
+--     imm2 = slice d7 d0 imm
+--     imm1 = imm ! 8
+--     imm0 = slice d18 d9 imm
+--     offset = unpack $ imm3 ++# imm2 ++# imm1 ++# imm0
--- src/ISA.hs
+++ src/ISA.hs
@@ -3,12 +3,34 @@ module ISA where
 import CLaSH.Prelude
 import CLaSH.Sized.Unsigned
 
-type Register = Unsigned 5
-type UImmediate = Unsigned 20
+type Register    = BitVector 5
+type Funct3      = BitVector 3
+type Funct7      = BitVector 7
+type Iimmediate  = BitVector 12
+type Simmediate  = BitVector 12
+type SBimmediate = BitVector 12
+type Uimmediate  = BitVector 20
+type UJimmediate = BitVector 20
+
+-- type Register    = Unsigned 5
+-- type Funct3      = Unsigned 3
+-- type Funct7      = Unsigned 7
+-- type Iimmediate  = Unsigned 12
+-- type Simmediate  = Unsigned 12
+-- type SBimmediate = Unsigned 12
+-- type Uimmediate  = Unsigned 20
+-- type UJimmediate = Unsigned 20
 
 data Instruction
-  = AUIPC Register UImmediate            -- AUIPC dest U-immedaite
-  | JAL Register (Unsigned 20)           -- JAL dest offset
-  | JALR Register Register (Unsigned 11) -- JALR dest base offset
-  | LUI Register UImmediate              -- LUI dest U-immediate
-  deriving (Show)
+  = Rtype  Funct3 Funct7 Register Register Register
+  | Itype  Funct3        Register Register          Iimmediate
+  | Stype  Funct3        Register Register          Simmediate
+  | SBtype Funct3        Register Register          SBimmediate
+  | Utype                Register                   Uimmediate
+  | UJtype               Register                   UJimmediate
+-- data Instruction
+--   = AUIPC Register UImmediate            -- AUIPC dest U-immedaite
+--   | JAL Register (Unsigned 20)           -- JAL dest offset
+--   | JALR Register Register (Unsigned 11) -- JALR dest base offset
+--   | LUI Register UImmediate              -- LUI dest U-immediate
+--   deriving (Show)
