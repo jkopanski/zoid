c2177a5e8298d35286fa3f0498f5f717d179f52b
Jakub Kopański <jkopanski@protonmail.com>
2018-09-28 10:32:43 +0200

    playing with state monad top level view of cpu

--- src/Zoid.hs
+++ src/Zoid.hs
@@ -1,12 +1,57 @@
+{-# language DataKinds  #-}
+{-# language GeneralizedNewtypeDeriving #-}
+{-# language TypeInType #-}
+{-# language DuplicateRecordFields #-}
 module Zoid where
 
 import Clash.Prelude
-import Instructions
+import Control.Monad.State
+import Data.Kind (Type)
+import Registers
+import Types
 
--- topEntity :: Signal (BitVector 32)
--- topEntity = decode
+data Config = Config
+  { registers :: Vec 32 (BitVector 32)
+  }
 
+newtype Processor a = Processor { process :: State Config a }
+  deriving ( Functor
+           , Applicative
+           , Monad
+           , MonadState Config
+           )
+
+class Arithmetic n where
+  add :: BitVector n -> BitVector n -> Processor (BitVector n)
+
+-- class Logic a where
+--   a == a :: Bool
+--   a /= a :: Bool
+--   not a :: a
+
+class Control a where
+  jump :: a
+
+class Load a where
+  load :: a
+
+class Store a where
+  store :: a
+
+type Immediate = BitVector 32
+
+
+decode :: BitVector 32 -> ISA
+decode = undefined
+
+-- instance Instruction IInstr where
+--   type Args IInstr = IFormat
+
+-- class Instruction i where
+--   type Args i :: * -> Format  
+--   execute :: i -> ProcM ()
+   
 topEntity
-  :: BitVector 32
-  -> Instr IOp
-topEntity = unpack
+  :: BitVector 7
+  -> BitVector 7
+topEntity = id
